cb5f8b721d92d38533cb25b8ec69eb3a460f6eb9
DavidEspinozaNemo <david1998es16@gmail.com>
2022-04-28 16:21:43 -0600

    cambios minimos

--- moleculer-demo/moleculer.config.js
+++ moleculer-demo/moleculer.config.js
@@ -58,8 +58,8 @@ module.exports = {
 	// More info: https://moleculer.services/docs/0.14/networking.html
 	// Note: During the development, you don't need to define it because all services will be loaded locally.
 	// In production you can set it via `TRANSPORTER=nats://localhost:4222` environment variable.
-	transporter: "kafka://localhost:22181", //"Kafka"
-	//transporter: null,
+	//transporter: "kafka://localhost:22181", //"Kafka"
+	transporter: null,
 
 	// Define a cacher.
 	// More info: https://moleculer.services/docs/0.14/caching.html
