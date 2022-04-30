4e5bf98a9bd83ed1b334fcec49093581df32cda7
DavidEspinozaNemo <david1998es16@gmail.com>
2022-04-29 19:51:10 -0600

    Update findrequest.service.js

--- moleculer-demo/services/findrequest.service.js
+++ moleculer-demo/services/findrequest.service.js
@@ -31,7 +31,7 @@ module.exports = {
     /*
      * modelos del adaptador
      */
-    adapter: new MongooseAdapter("mongodb://localhost:27017/solicitud"),
+    adapter: new MongooseAdapter("mongodb://localhost:27018/solicitud"),
 	model: mongoose.models.solicitud || mongoose.model("solicitudes", mongoose.Schema({
         id_solicitud : { type: String },
         id_contenedor : { type: String },
@@ -57,35 +57,31 @@ module.exports = {
 			/** @param {Context} ctx  */
 			async handler(ctx) {
                 const contdb = 4
-                const data = ctx.params.id_solicitud;
-                const base1 = "mongodb://localhost:MexicoMov"
-                const base2 = "mongodb://localhost:CostaRicaMov"
-                const base3 = "mongodb://localhost:PanamaMov"
-                const base4 = "mongodb://localhost:ColombiaMov"
+                //const data = ctx.params.id_solicitud;
                 for( let i = 1; i <= contdb; i++) {
                     switch(i){
                         case 1:
-                            const con01 = new MongooseAdapter("mongodb://"+base1);
+                            const con01 = new MongooseAdapter("mongodb://27018:mexicomov/solicitud");
                             const resp01 = await con01.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp01));
+                            //await ctx.broker.cacher.set(data, JSON.stringify(resp01));
 					        console.log("Datos base de datos");
 					        return resp01;
                         case 2:
-                            const con02 = new MongooseAdapter("mongodb://"+base2);
+                            const con02 = new MongooseAdapter("mongodb://27019:costaricamov/solicitud");
                             const resp02 = await con02.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp02));
+                            //await ctx.broker.cacher.set(data, JSON.stringify(resp02));
 					        console.log("Datos base de datos");
 					        return resp02;
                         case 3:
-                            const con03 = new MongooseAdapter("mongodb://"+base3);
+                            const con03 = new MongooseAdapter("mongodb://27020:panamamov/solicitud");
                             const resp03 = await con03.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp03));
+                            //await ctx.broker.cacher.set(data, JSON.stringify(resp03));
 					        console.log("Datos base de datos");
 					        return resp03;
                         case 4:
-                            const con04 = new MongooseAdapter("mongodb://"+base4);
+                            const con04 = new MongooseAdapter("mongodb://27021:colombiamov/solicitud");
                             const resp04 = await con04.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp04));
+                            //await ctx.broker.cacher.set(data, JSON.stringify(resp04));
 					        console.log("Datos base de datos");
 					        return resp04;    
                         default:
