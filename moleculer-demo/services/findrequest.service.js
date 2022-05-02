d047175e2d2f409aea225f58d787def346ef2abb
DavidEspinozaNemo <david1998es16@gmail.com>
2022-05-02 14:31:31 -0600

    Update findrequest.service.js

--- moleculer-demo/services/findrequest.service.js
+++ moleculer-demo/services/findrequest.service.js
@@ -28,18 +28,6 @@ module.exports = {
 
     mixins: [DbService],
 
-    /*
-     * modelos del adaptador
-     */
-    //adapter: new MongooseAdapter("mongodb://localhost:27017/solicitud"),
-    /*adapter: new MongooseAdapter("mongodb://localhost:27051/movimiento"),
-	model: mongoose.models.solicitud || mongoose.model("Solicitud", mongoose.Schema({
-        id_solicitud : { type: Number },
-        id_contenedor : { type: Number },
-        paisllegada : { type: String },
-        activo : { type: Boolean}
-    }, { versionKey: false })),*/
-
 	/**
 	 * Actions
 	 */
@@ -57,62 +45,32 @@ module.exports = {
             },
 			/** @param {Context} ctx  */
 			async handler(ctx) {
-                const contdb = 2
                 const data = ctx.params.id_solicitud;
                 var resp;
                 
-                //const base1 = "mongodb://localhost:MexicoMov"
-                //const base2 = "mongodb://localhost:CostaRicaMov"
-                // modelo
-                const myschema = new mongoose.Schema({"id_solicitud" : Number, "id_contenedor": Number, "paisllegada": String, "activo": Boolean});
-                //const Solicitud = mongoose.model('Solicitud', myschema);
+                var conecciones = ['25.77.46.90:27020/mov_costarica',
+                    '25.5.181.178:27020/mov_panama',
+                    '25.77.2.238:27020/mov_colombia',
+                    '25.77.226.95:27020/mov_mexico',
+                ];
+                const contdb = conecciones.length;
 
-                for( let i = 1; i <= contdb; i++) {
-                    switch(i){
-                        case 1:
-                            /*
-                            const con01 = new MongooseAdapter("mongodb://"+base1);
-                            const resp01 = await con01.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp01));
-					        console.log("Datos base de datos");
-					        return resp01;*/
-                            //connection
-                            const connection01 = mongoose.createConnection('mongodb://localhost:27051/movimiento');
-                            const Solicitud01 = connection01.model('Solicitud', myschema);
-                            resp = resp + await Solicitud01.find({"id_solicitud" : data});
-                            break;
-                        case 2:
-                            /*
-                            const con02 = new MongooseAdapter("mongodb://"+base2);
-                            const resp02 = await con02.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp02));
-					        console.log("Datos base de datos");
-					        return resp02;*/
-                            const connection02 = mongoose.createConnection('mongodb://localhost:27052/movimiento');
-                            const Solicitud02 = connection02.model('Solicitud', myschema);
-                            resp = resp + await Solicitud02.find({"id_solicitud" : data});
-                            break;
-                        case 3:
-                            /*
-                            const con03 = new MongooseAdapter("mongodb://"+base3);
-                            const resp03 = await con03.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp03));
-					        console.log("Datos base de datos");
-					        return resp03;*/
-                            break;
-                        case 4:
-                            /*
-                            const con04 = new MongooseAdapter("mongodb://"+base4);
-                            const resp04 = await con04.find({"idsolicitud": ctx.params.id_solicitud, "activo": true})
-                            await ctx.broker.cacher.set(data, JSON.stringify(resp04));
-					        console.log("Datos base de datos");
-					        return resp04;  */
-                            break;
-                        default:
-                            return null;
+                for( let i = 0; i < contdb; i++) {
+                    var connection = mongoose.createConnection("mongodb://"+conecciones[i]);
+                    const myschema = connection.model.solicitud || connection.model("solicitud", mongoose.Schema({
+                        id_solicitud: { type: Number },
+                        id_contenedor: { type: Number },
+                        paisllegada: { type: String },
+                        activo: { type: Boolean }
+                    }));
+                    var resp = await myschema.find({"id_solicitud" : data, "activo": true});
+                    if(resp[0].id_solicitud == data && resp[0].activo ){
+                        return resp;
                     }
-                }
-                return resp;
+                    connection.close();
+                };
+                return "No se encontro la información";
+
             }
         }
 
