cccc0fb16073dcf152d8fe445787a2e92d0970b7
Esteban <estebanguzra@gmail.com>
2022-05-02 14:25:01 -0600

    update conections

--- microservirces/services/reserve.service.js
+++ microservirces/services/reserve.service.js
@@ -32,41 +32,6 @@ module.exports = {
 
 	mixins: [DbService],
 
-	adapter: new MongooseAdapter("mongodb://localhost:27017/puertos"),
-	model:
-		mongoose.models.contenedores ||
-		mongoose.model(
-			"contenedores",
-			mongoose.Schema(
-				{
-					id_contenedor: { type: Number },
-					fecha_limite: { type: String },
-					pais_A: { type: String },
-					pais_B: { type: String },
-					espacios: { type: Array },
-					precio_kilogramo: { type: Number },
-					peso_maximo: { type: Number },
-					activo: { type: Boolean}
-				},
-				{ versionKey: false }
-			) 
-		) ||
-		mongoose.model(
-			"solicitudes",
-			mongoose.Schema(
-				{
-					id_solicitud: { type: Number },
-					id_contenedor: { type: Number },
-					pais_llegada: { type: String },
-					id_cliente: { type: Number },
-					peso: { type: Number },
-					activo: { type: Boolean}
-				},
-				{ versionKey: false }
-			)
-		)
-	,
-
 	/**
 	 * Actions
 	 */
@@ -91,8 +56,32 @@ module.exports = {
 					return "Faltan datos";
 				}
 				//buscar el contenedor *****
-				const Cont = mongoose.models.contenedores;
-				const resp = await Cont.find({"id_contenedor":ctx.params.container_id, "activo": true});
+				var bds=["25.77.46.90:27019,25.5.181.178:27021/puertos?replicaSet=rep_pais_costarica",
+				"25.5.181.178:27019,25.77.2.238:27021/puertos?replicaSet=rep_pais_panama",
+				"25.77.2.238:27019,25.77.226.95:27021/puertos?replicaSet=rep_pais_colombia",
+				"25.77.226.95:27019,25.77.46.90:27021/puertos?replicaSet=rep_pais_mexico"];
+				var selectedDB;
+				if (request.country === "Costa Rica"){
+					selectedDB = bds[0];
+				} else if (request.country === "Panamá"){
+					selectedDB = bds[1];
+				} else if (request.country === "Colombia"){
+					selectedDB = bds[2];
+				} else {
+					selectedDB = bds[3];
+				}
+				var est = mongoose.createConnection("mongodb://" + selectedDB);
+				const model01 = est.models.contenedores || est.model("contenedores", mongoose.Schema({
+					id_contenedor: { type: Number },
+					fecha_limite: { type: String },
+					pais_A: { type: String },
+					pais_B: { type: String },
+					espacios: { type: Array },
+					precio_kilogramo: { type: Number },
+					peso_maximo: { type: Number },
+					activo: { type: Boolean}
+				})); 
+				const resultado = await model01.find({"id_contenedor":ctx.params.container_id, "activo": true});
 				if(resultado == null){
 					return "Contenedor ocupado";
 				}
@@ -103,6 +92,29 @@ module.exports = {
 				await resp.save();
 				//set el redis *****
 				// guardar en mov
+				var bds_mov=["25.77.46.90:27020,25.5.181.178:27022/movimiento?replicaSet=rep_mov_costarica",
+				"25.5.181.178:27020,25.77.2.238:27022/movimiento?replicaSet=rep_mov_panama",
+				"25.77.2.238:27020,25.77.226.95:27022/movimiento?replicaSet=rep_mov_colombia",
+				"25.77.226.95:27020,25.77.46.90:27022/movimiento?replicaSet=rep_mov_mexico"];
+				var selectedMov;
+				if (request.country === "Costa Rica"){
+					selectedMov = bds_mov[0];
+				} else if (request.country === "Panamá"){
+					selectedMov = bds_mov[1];
+				} else if (request.country === "Colombia"){
+					selectedMov = bds_mov[2];
+				} else {
+					selectedMov = bds_mov[3];
+				}
+				var est2 = mongoose.createConnection("mongodb://" + selectedMov);
+				const model02 = est2.models.solicitudes || est2.model("solicitudes", mongoose.Schema({
+					id_solicitud: { type: Number },
+					id_contenedor: { type: Number },
+					pais_llegada: { type: String },
+					id_cliente: { type: Number },
+					peso: { type: Number },
+					activo: { type: Boolean}
+				})); 
 				const doc = new mongoose.models.solicitudes({
 					id_solicitud: json_resp.id_contenedor,
 					id_contenedor: json_resp.id_contenedor,
@@ -111,7 +123,6 @@ module.exports = {
 					id_cliente: request.id_client,
 					activo: true
 				});
-				//debería cambiar la base en la que guardo ****
 				await doc.save();
 				//all is ok
 				return "Ok";
